ec619e055f3dd66388a892eb6b610fdedb327718
JKFullDev <jcah009@gmail.com>
2026-01-05 02:08:08 +0100

    Estructura inicial del proyecto y rutas + Header y Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className='footer'>
+            Portafolio Juan Carlos Alonso Hernando &copy; 2025
+        </footer>
+    )
+}
